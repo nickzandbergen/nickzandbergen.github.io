b0df512c2a16e77ca5f2172bfce87c3acf8585f2
Nick Zandbergen <nickzandbergen@users.noreply.github.com>
2016-09-09 19:40:50 -0700

    Update and rename game.js to pattern.js

---
+++ pattern.js
@@ -0,0 +1,64 @@
+var squares = 20;//change this! 
+var size;
+var rotation;
+
+
+function setup() {
+    createCanvas(480, 720);
+    noStroke();
+    angleMode(DEGREES);
+    size = width/squares;
+    var rotate = 128 ;
+    
+}
+
+function draw() {
+    
+    
+    background(128);
+    
+    drawSquaresSquared(rotate, false);
+    drawSquaresSquared(rotate, true);
+
+    
+    if (rotate % 360 == 0)
+        rotate = 0;
+    
+    
+} //end of draw 
+
+
+function drawSquaresSquared(rotation, black) {
+    var offset = 0;
+    if(!black)
+        offset = size * Math.SQRT2 / 2;
+        
+        
+    
+    for(var i = 0; i < width; i += size * 2 * Math.SQRT2) {
+        for(var j = 0; j < height; j += size * 2 * Math.SQRT2) {
+            drawSquare(i + offset, j + offset, rotation , black)
+            
+            
+            
+            
+            
+        }
+    }
+    
+    
+}
+function drawSquare(x, y, rotation, black) {
+    push();
+    translate(x,y);
+    rotate(rotation);
+    if (black) {
+       fill(0); 
+    } else {
+        fill(255);
+    }
+    rect(0,0,size, size);
+    
+    pop();
+    
+}
