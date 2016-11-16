78fdefc0bf2cbc694d6eb122a84e7003f7f4a45b
Nick Zandbergen <nickzandbergen@users.noreply.github.com>
2016-11-16 15:15:12 -0800

    Update pattern.js

--- pattern.js
+++ pattern.js
@@ -13,11 +13,11 @@ function setup() {
 function draw() {
     
     if(frameCount % 360 >= 90) {
-        background(255);
+        background(128);
         drawSquaresSquared(true);
         drawSquaresSquared(false);
     } else if(frameCount % 360 >= 180) {
-        background(0);
+        background(128);
         drawSquaresSquared(false);
         drawSquaresSquared(true);
     //} else if(frameCount % 360 >= 270) {
