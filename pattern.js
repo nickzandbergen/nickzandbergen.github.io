46c6454784f3d7d173a6a781d4213c1c701fa2a7
Nick Zandbergen <nickzandbergen@users.noreply.github.com>
2017-04-21 13:45:25 -0700

    Update pattern.js

--- pattern.js
+++ pattern.js
@@ -1,21 +1,20 @@
-var squares = 8; //change this
-var size;
-
+var distMax;
 
 function setup() {
     createCanvas(1920, 1080);
     noStroke();
     angleMode(DEGREES);
     rectMode(CENTER);
-    size = width/squares;
+    distMax = mag(width/2, height/2);
 }
 
 function draw() {
-  if(mouseIsPressed)
-  {
-    fill((mouseX + mouseY) % 255);
+    
+    
+    var red = map(mouseX, 0, width, 0, 255);
+    var grn = map(mouseY, 0, height, 0, 255);
+    var blu = map(dist(mouseX, mouseY, width/2, height/2), 0, distMax, 0, 255);
+    fill(red, grn, blu);
     ellipse(mouseX, mouseY, 45, 45);
-  }
-        
-
+  
 } 
